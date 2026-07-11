ce6e78f182d3e05031ffb55f89a5610885d53652
mjak88 <76479953+mjak88@users.noreply.github.com>
2026-07-11 15:59:26 +0300

    Update games-manifest.js

--- games-manifest.js
+++ games-manifest.js
@@ -1,7 +1,5 @@
 // games-manifest.js
 // Whenever you build a new game, just drop its folder name into this array!
 const ACTIVE_GAMES_MANIFEST = [
-    "maze",
-    "elements",
-    "claw"
+    "saiyanpower"
 ];
